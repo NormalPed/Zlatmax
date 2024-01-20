506dcd7cca82f860fd719e5763a594285cd8b483
aboba <maksshos05@gmail.com>
2024-01-20 18:55:17 +0200

    full adaptive product card. Project builded

--- src/js/app.js
+++ src/js/app.js
@@ -46,7 +46,7 @@ flsFunctions.spollers();
 Документация: https://template.fls.guru/template-docs/modul-taby.html
 Сниппет (HTML): tabs
 */
-// flsFunctions.tabs();
+flsFunctions.tabs();
 
 /*
 Модуль "показать еще"
@@ -83,7 +83,7 @@ flsForms.formFieldsInit({ viewPass: false });
 flsForms.formSubmit();
 
 /* Модуль формы "колличество" */
-// flsForms.formQuantity();
+flsForms.formQuantity();
 
 /* Модуль звездного рейтинга */
 flsForms.formRating();
